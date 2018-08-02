cac283dcd4953ccd5eb1b9c9fd332fe5a0920841
Mohammad Dayyan <mds_soft@yahoo.com>
2018-08-02 17:17:19 +0430

    Remove BrowserModule from import section

--- projects/mds-angular-persian-date-time-picker/src/lib/mds-angular-persian-date-time-picker.module.ts
+++ projects/mds-angular-persian-date-time-picker/src/lib/mds-angular-persian-date-time-picker.module.ts
@@ -5,7 +5,6 @@ import { MatInputModule } from '@angular/material';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatGridListModule } from '@angular/material/grid-list';
-import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MdsAngularPersianDateTimePickerCoreComponent } from './core/mds-angular-persian-date-time-picker-core.component';
 import { MdsAngularPersianDateTimePickerComponent } from './mds-angular-persian-date-time-picker.component';
@@ -14,7 +13,7 @@ import { SafeHtmlPipe } from './pipes/safe-html.pipe';
 
 @NgModule({
   imports: [
-    CommonModule, BrowserModule, BrowserAnimationsModule,
+    CommonModule, BrowserAnimationsModule,
     MatFormFieldModule, MatButtonModule, MatInputModule,
     MatGridListModule,  FormsModule,
   ],
