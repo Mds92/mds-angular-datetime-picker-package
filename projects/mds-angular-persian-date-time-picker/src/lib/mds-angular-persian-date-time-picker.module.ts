6dfca9f4efd9e5a1ba421bb27562fd34e6403d55
Mohammad Dayyan <mds_soft@yahoo.com>
2018-09-19 21:30:05 +0430

    fix a bug

--- projects/mds-angular-persian-date-time-picker/src/lib/mds-angular-persian-date-time-picker.module.ts
+++ projects/mds-angular-persian-date-time-picker/src/lib/mds-angular-persian-date-time-picker.module.ts
@@ -5,7 +5,6 @@ import { MatInputModule } from '@angular/material';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatGridListModule } from '@angular/material/grid-list';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MdsAngularPersianDateTimePickerCoreComponent } from './core/mds-angular-persian-date-time-picker-core.component';
 import { MdsAngularPersianDateTimePickerComponent } from './mds-angular-persian-date-time-picker.component';
 import { PersianNumberPipe } from './pipes/persian-number.pipe';
@@ -13,7 +12,7 @@ import { SafeHtmlPipe } from './pipes/safe-html.pipe';
 
 @NgModule({
   imports: [
-    CommonModule, BrowserAnimationsModule,
+    CommonModule,
     MatFormFieldModule, MatButtonModule, MatInputModule,
     MatGridListModule, FormsModule, ReactiveFormsModule
   ],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { MdsAngularPersianDateTimePickerModule } from 'projects/mds-angular-persian-date-time-picker/src/lib/mds-angular-persian-date-time-picker.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule, FormsModule, ReactiveFormsModule,
     MdsAngularPersianDateTimePickerModule, 
   ],
